c94b64056943c7685154723be2a88803d69b7fb4
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-02-17 20:59:53 +0300

    set up profile endpoint

---
+++ server/api/controllers/profile.controller.js
@@ -0,0 +1,13 @@
+const User = require('../models/user/user.model.js');
+
+module.exports.viewProfile = (req, res) => {
+	if (!req.payload._id) {
+		res.status(401).json({
+			message: 'Unauthorized to view this resource'
+		})
+	}
+
+	User.findById(req.payload._id).exec((err, user) => {
+		res.status(200).json(user);
+	});
+};
