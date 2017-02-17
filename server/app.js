d8c905fab320e8206e49d3fc7bbd34b92470ac4c
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-02-17 20:52:51 +0300

    add required files

--- server/app.js
+++ server/app.js
@@ -1,31 +1,36 @@
-const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const express = require('express');
+const passport = require('passport');
+const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const passport = require('passport');
 
-// require('./config/passport.service.js');
+require('./api/models/db.js');
+require('./config/passport.config.js')(passport);
 
+const apiRoutes = require('./api/routes/api.js');
 
 const app = express();
 
-app.use(logger('dev'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use(cookieParser());
-
 app.use(express.static(path.join(__dirname, '../client')));
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
 app.use(passport.initialize());
+
+app.use('/api', apiRoutes);
+
+// app.use(passport.session());
 
 /* development error handler
  * will print stacktrace
  */
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use((err, req, res, next) => {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
