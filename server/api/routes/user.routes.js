ec43f135c42df5d8237fd7f3fb8bafdd9727b47e
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-02-18 12:24:14 +0300

    define routes and include controllers

--- server/api/routes/user.routes.js
+++ server/api/routes/user.routes.js
@@ -1,8 +1,23 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-	res.send('some resource');
+const router = require('express').Router();
+const jwt = require('express-jwt');
+const auth = jwt({
+	secret: 'SuperSecret',
+	userProperty: 'payload'
 });
+
+const profileCtrl = require('../controllers/profile.controller.js');
+const authCtrl = require('../controllers/auth.controller.js');
+
+
+router.get('/', (req, res, next) => {
+	res.status(200).json({ message: 'User!' });
+});
+
+// profile
+router.get('/profile', auth, profileCtrl.viewProfile);
+
+// authentication
+router.post('/signup', authCtrl.signup);
+router.post('/login', authCtrl.login);
 
 module.exports = router;
