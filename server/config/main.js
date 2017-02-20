28566e823c29f2e867a09685f8a1a8e8f228c256
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-02-20 19:28:44 +0300

    set secret to string

--- server/config/main.js
+++ server/config/main.js
@@ -1,6 +1,6 @@
 module.exports = {  
   // Secret key for JWT signing and encryption
-  'secret': process.env.SECRET,
+  'secret': 'super_secret',
   'database': 'mongodb://localhost:27017/mean-auth-starter',
   
 }
