2c601bd29a45e2fcc5d7552ed2883435a39786ca
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-02-21 22:04:33 +0300

    refactor functions for readability

--- client/app/auth/login/controllers/login.controller.js
+++ client/app/auth/login/controllers/login.controller.js
@@ -1,20 +1,23 @@
-angular.module('meanAuthStarter.login', [])
-.controller('loginCtrl', ['authService', (authService) => {
-	
-	const user = this;
-	user.details = {
-		email: '',
-		password: ''
-	};
+(function () {
+	function loginCtrl (authService) {
+		const user = this;
+		user.details = {
+			email: '',
+			password: ''
+		};
 
-	user.onSubmit = () => {
-		authService
-			.loginUser(user.details)
-			.error((err) => {
-				alert(err);
-			})
-			.then( () => {
-				state.go('profile');
-			});
-	};
-}])
+		user.onSubmit = () => {
+			authService
+				.loginUser(user.details)
+				.error((err) => {
+					alert(err);
+				})
+				.then( () => {
+					state.go('profile');
+				});
+		};
+	}
+	angular.module('meanAuthStarter.login', [])
+	.controller('loginCtrl', ['authService', loginCtrl]);
+})();
+
