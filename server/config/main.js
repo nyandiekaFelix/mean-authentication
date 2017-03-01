e999c31d683e2975a8b6cfd889c07fb3443ad3da
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-01 13:12:12 +0300

    increase indentation

--- server/config/main.js
+++ server/config/main.js
@@ -1,6 +1,5 @@
 module.exports = {  
-  // Secret key for JWT signing and encryption
-  'secret': 'super_secret',
-  'database': 'mongodb://localhost:27017/mean-auth-starter',
-  
+  	// Secret key for JWT signing and encryption
+    'secret': 'super_secret',
+    'database': 'mongodb://localhost:27017/mean-auth-starter',
 }
