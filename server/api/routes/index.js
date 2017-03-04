3a1c6020c185e0fe2ca2c0d06e5a686631c89c05
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-04 20:56:48 +0300

    require user controller file and set base user route

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user.routes.js');
+const userCtrl = require('../controllers/user.controller.js');
 
-router.get('/', (req, res) => {
- 	res.status(200).json({ message: 'Api Connected!' });
-});
-
-router.use('/account', userRoutes);
+// All user endpoints to be preceded by '/user'
+router.use('/user', userRoutes);
 
 module.exports = router;
 
