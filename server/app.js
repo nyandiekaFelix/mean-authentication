79e5055148e26f1acd17c95011b1e514c8b5be0f
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-04 20:55:34 +0300

    requre dotenv module and set base api route

--- server/app.js
+++ server/app.js
@@ -1,14 +1,9 @@
 const path = require('path');
 const logger = require('morgan');
 const express = require('express');
-const passport = require('passport');
-// const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 
-require('./api/models/db.js');
-require('./config/passport.config.js')(passport);
-
+require('dotenv').load();
 
 const app = express();
 
@@ -18,51 +13,10 @@ app.use('/bower_components', express.static( path.dirname(__dirname) + '/bower_c
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-
-app.use(passport.initialize());
 
 const apiRoutes = require('./api/routes/index.js');
-app.use('/', apiRoutes);
+app.use('/api', apiRoutes);
 
 
-app.use( (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'index.html'));
-});
-
-/*
- * Unauthorozed Errors
- */
-app.use( (err, req, res, next) => {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).json({
-            message: `${err.name} : ${err.message}`
-        });
-    }
-});
-
-/* development error handler
- * will print stacktrace
- */
-if (app.get('env') === 'development') {
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-/* production error handler
- * no stacktraces leaked to user
- */
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 module.exports = app;
