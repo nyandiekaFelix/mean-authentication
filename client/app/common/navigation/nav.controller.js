4004ecd1b1f253daafe2e1241842217bc34e9c9b
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-05 11:39:23 +0300

    delete 'common' folder

--- client/app/common/navigation/nav.controller.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	function navCtrl(authService) {
-		const user = this;
-		user.isLoggedIn = authService.isLoggedIn();
-		user.getUserDetails = authService.getUserDetails();
-	}
-	angular.module('meanAuthStarter.nav', [])
-	.controller('navCtrl', navCtrl);
-})();
--- client/app/common/navigation/nav.directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('meanAuthStarter')
-.directive('navigation',() =>{
-	
-	return {
-		restrict: 'EA',
-		templateUrl: 'app/common/navigation/nav.html',
-		controller: 'navCtrl as navuser'
-	};
-});
