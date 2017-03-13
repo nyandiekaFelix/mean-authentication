99c14ea1b65da0d3496bd9413705183fd3e43a16
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-13 13:22:23 +0300

    inject module dependencies

--- client/app/main.js
+++ client/app/main.js
@@ -1,46 +1,26 @@
 const app = angular.module('meanAuthStarter', 
 	['ui.router',
-	 'meanAuthStarter.auth',
+	 'ngStorage',
+	 'angularMoment',
+	 'ngSanitize',
+	 'toastr',
+	 'satellizer',
+	 'meanAuthStarter.nav',
+	 'meanAuthStarter.routes',
 	 'meanAuthStarter.signup',
 	 'meanAuthStarter.login',
-	 'meanAuthStarter.nav']);
+	 'meanAuthStarter.profile',
+	 'meanAuthStarter.logout'
+	]);
 
-app.config(($stateProvider, $urlRouterProvider) => {
-	$urlRouterProvider.otherwise('/');
+app.config(['$authProvider', '$httpProvider', defaultConfig]);
 
-	$stateProvider
-		.state('home', {
-			url: '/',
-			templateUrl: 'app/home/home.html',
-			// controller: 'homeCtrl'
-		})
-
-		.state('login', {
-			url: '/login',
-			templateUrl: 'app/auth/login/login.html',
-			controller: 'loginCtrl'
-		})
-
-		.state('signup', {
-			url: '/signup',
-			templateUrl: 'app/auth/signup/signup.html',
-			controller: 'signupCtrl'
-		})
-
-		.state('profile', {
-			url: '/profile',
-			templateUrl: 'app/account/profile/profile.html',
-			controller: 'profileCtrl',
-			module: 'private'
-		});
-});
-
-app.run(['$rootScope', '$state', 'authService', ($rootScope, $state, authService) => {
-	$rootScope.$on('$stateChangeStart', (event, toState, toParams, fromState, fromParams) => {
-		if (toState.module === 'private' && !authService.isLoggedIn()) {
-			event.preventDefault();
-			$state.go('login');
-		}
-	});
-}])
+function defaultConfig($authProvider, $httpProvider) {
+	$authProvider.baseUrl = '/';
+	$authProvider.loginUrl = '/api/user/login';
+	$authProvider.signupUrl = '/api/user/register';
+	$authProvider.authHeader = 'Authorization';
+	$authProvider.authToken = 'Bearer';
+	$authProvider.storageType = 'localStorage';
+}
 
