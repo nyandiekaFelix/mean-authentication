efadd4ea12b8fea2daec8805e248d09758c6898d
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-13 13:32:47 +0300

    remove non-required method

--- server/app.js
+++ server/app.js
@@ -10,14 +10,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../client')));
 app.use('/bower_components', express.static( path.dirname(__dirname) + '/bower_components'));
 
-/* 
- * Handling angular requests
- * Page changes will be handled on the front-end
- */
-app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'index.html'));
-});
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
