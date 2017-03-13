5114b82970e457abd42ccdd49e56fb8b99a33d5b
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-13 13:20:15 +0300

    update frontend routes

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -1,59 +1,66 @@
-(() => {
-	const app = angular.module('meanAuthStarter.routes', []);
-	app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', routes]);
+angular.module('meanAuthStarter.routes', [])
+	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', routes]);
 
-	function routes($stateProvider, $urlRouterProvider, $locationProvider) {
-		$urlRouterProvider.otherwise('/');
-		$locationProvider.html5Mode(true);
+function routes($stateProvider, $urlRouterProvider, $locationProvider) {
+	$urlRouterProvider.otherwise('/');
+	// $locationProvider.html5Mode(true);
 
-		$stateProvider
-			.state('home', {
-				url: '/',
-				templateUrl: 'app/home/home.html',
-				// controller: 'homeCtrl',
-			})
+	const skipIfLoggedIn = ['$q', '$auth', function($q, $auth){
+		const deferred = $q.defer();
 
-			.state('login', {
-				url: '/login',
-				templateUrl: 'app/auth/login/login.html',
-				controller: 'loginCtrl',
-			})
+		if ($auth.isAuthenticated()) {
+			deferred.reject();
+		}
 
-			.state('register', {
-				url: '/register',
-				templateUrl: 'app/auth/signup/signup.html',
-				controller: 'signupCtrl',
-			})
+		deferred.resolve();
 
-			.state('profile', {
-				url: '/profile',
-				templateUrl: 'app/account/profile/profile.html',
-				controller: 'profileCtrl',
-			});
+		return deferred.promise;
+	}];
 
-		// Reject a promise if the user is logged in
-		function skipIfLoggedIn($q, $auth) {
-			const deferred = $q.defer();
+	const loginRequired = ['$q', '$state', '$auth', function($q, $state, $auth){
+		const deferred = $q.defer();
 
-			if($auth.isAuthenticated()) {
-				deferred.reject();
-			}
+		if ($auth.isAuthenticated()) {
 			deferred.resolve();
-
-			return deferred.promise;
 		}
 
-		// Redirect user to login page if not logged in
-		function loginRequired($q, $state, $auth) {
-			const deferred = $q.defer();
+		$state.go('login');
 
-			if ($auth.isAuthenticated()) {
-				deferred.resolve();
-			}
+		return deferred.promise;
+	}];
 
-			$state.go('login');
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'app/home/home.html'
+			// controller: 'homeCtrl',
+		})
 
-			return deferred.promise;
-		}
-	}
-})();
+		.state('login', {
+			url: '/login',
+			templateUrl: 'app/auth/login/login.html',
+			controller: 'loginCtrl',
+			resolve: { skipIfLoggedIn: skipIfLoggedIn }
+		})
+
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'app/auth/signup/signup.html',
+			controller: 'signupCtrl',
+			resolve: { skipIfLoggedIn: skipIfLoggedIn }
+		})
+
+		.state('profile', {
+			url: '/profile',
+			templateUrl: 'app/account/profile/profile.html',
+			controller: 'profileCtrl',
+			resolve: { loginRequired: loginRequired }
+		})
+
+		.state('logout', {
+			url: '/logout',
+			templateUrl: null,
+			controller: 'logoutCtrl',
+			resolve: { loginRequired: loginRequired }
+		});
+}
