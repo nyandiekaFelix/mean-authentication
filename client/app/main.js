3cd9d4a64d44f32204ef92d2d6c73c208cb1da5f
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-16 19:53:20 +0300

    remove unused modules

--- client/app/main.js
+++ client/app/main.js
@@ -1,26 +1,22 @@
-const app = angular.module('meanAuthStarter', 
-	['ui.router',
-	 'ngStorage',
-	 'angularMoment',
-	 'ngSanitize',
-	 'toastr',
-	 'satellizer',
-	 'meanAuthStarter.nav',
-	 'meanAuthStarter.routes',
-	 'meanAuthStarter.signup',
-	 'meanAuthStarter.login',
-	 'meanAuthStarter.profile',
-	 'meanAuthStarter.logout'
-	]);
+const app = angular.module('meanAuthStarter', ['ui.router',
+    'ngSanitize',
+    'toastr',
+    'satellizer',
+    'meanAuthStarter.nav',
+    'meanAuthStarter.routes',
+    'meanAuthStarter.signup',
+    'meanAuthStarter.login',
+    'meanAuthStarter.profile',
+    'meanAuthStarter.logout'
+]);
 
 app.config(['$authProvider', '$httpProvider', defaultConfig]);
 
 function defaultConfig($authProvider, $httpProvider) {
-	$authProvider.baseUrl = '/';
-	$authProvider.loginUrl = '/api/user/login';
-	$authProvider.signupUrl = '/api/user/register';
-	$authProvider.authHeader = 'Authorization';
-	$authProvider.authToken = 'Bearer';
-	$authProvider.storageType = 'localStorage';
+    $authProvider.baseUrl = '/';
+    $authProvider.loginUrl = '/api/user/login';
+    $authProvider.signupUrl = '/api/user/register';
+    $authProvider.authHeader = 'Authorization';
+    $authProvider.authToken = 'Bearer';
+    $authProvider.storageType = 'localStorage';
 }
-
