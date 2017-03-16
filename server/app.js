cf4fbcbe935086fbaaa66cc7dc32705476132c89
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-16 19:56:48 +0300

    enable cors

--- server/app.js
+++ server/app.js
@@ -1,18 +1,23 @@
 const path = require('path');
+const cors = require('cors');
 const logger = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-require('dotenv').load();
-
 const app = express();
 
+// Enable Cross Origin Resource Sharing
+app.use(cors());
+
+// Serve frontend files
 app.use(express.static(path.join(__dirname, '../client')));
-app.use('/bower_components', express.static( path.dirname(__dirname) + '/bower_components'));
+app.use('/bower_components', express.static(path.dirname(__dirname) + '/bower_components'));
 
 app.use(logger('dev'));
+
+// Handle POST request data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const apiRoutes = require('./api/routes/index.js');
 app.use('/api', apiRoutes);
