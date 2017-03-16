9ef6139c512444a39a9e47d10d63c9336b00c728
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-16 19:56:21 +0300

    remove unused controller

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./user.routes.js');
-const userCtrl = require('../controllers/user.controller.js');
 
 // All user endpoints to be preceded by '/user'
 router.use('/user', userRoutes);
 
 module.exports = router;
-
