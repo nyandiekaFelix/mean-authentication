71e6294dc502670b1e2db5990e3f8a0d38372298
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-04-17 15:09:53 +0300

    remove rudundant file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
-const app = require('./app.js');
-const config = require('./config/secrets.js');
+const path = require('path');
+const logger = require('morgan');
+const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-
+const config = require('./config/secrets.js');
 const PORT = config.port;
+
+const app = express();
+
+// Serve frontend files
+app.use(express.static(path.join(__dirname, '../client')));
+app.use('/bower_components', express.static(path.dirname(__dirname) + '/bower_components'));
+
+app.use(logger('dev'));
+
+// Handle POST request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const apiRoutes = require('./api/routes/index.js');
+app.use('/api', apiRoutes);
+
 mongoose.connect(config.database);
 
-// start the server
 app.listen(PORT, () => {
     console.log(`Magic on localhost:${PORT}`);
 });
