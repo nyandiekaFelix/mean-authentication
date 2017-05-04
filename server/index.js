f3c43b3e6e2fe0cc19a588cfb68fd665a86a6669
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-04 18:16:34 +0300

    changed folder structure

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const config = require('./config/secrets.js');
+const config = require('./config/secrets');
 const PORT = config.port;
 
 const app = express();
