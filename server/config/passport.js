e979d946628a308c436398d55b9ab90cdb32d00d
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-07 20:17:46 +0300

    uncomment required module

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,9 +1,30 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
-const secrets = require('./secrets.js');
+
+const config = require('./env');
+const User = require('../models/user.model');
 
 module.exports = (passport) => {
-    let options = {};
-    options.jwtFromRequest = 
+    let options = {
+        jwtFromRequest: ExtractJwt.fromAuthHeader(),
+        secretOrKey: config.TOKEN_SECRET
+    };
+
+    passport.use(new JwtStrategy(options, (jwt_payload, next) => {
+        User.findOne({id: jwt_payload.sub})
+            .exec()
+            .then(user => {
+                if (user) {
+                    return next(null, user);
+                } else {
+                    return next(null, false);
+                    //or create a new account
+                }
+            })
+            .catch(err => {
+                return next(err, false);
+            });
+    }));
+   
 }
