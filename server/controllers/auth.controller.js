6963ea6260763a29b584fe98af2ee5fb3d97fa9b
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-10 12:36:26 +0300

    add register and login methods

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,13 +3,71 @@ const jwt =  require('jsonwebtoken');
 const config = require('../config/env');
 const User = require('../models/user.model');
 
-function generateToken(user) {
+function generateJWT(user) {
     return jwt.sign(user, config.TOKEN_SECRET, {
         expiresIn: 600000 // in seconds
     });
 }
 
 module.exports = {
-    register: (req, res) => {},
-    login: (req, res) => {}
+     registerUser: (req, res) => {
+        User.findOne({ email: req.body.email })
+            .exec()
+            .then(existingUser => {
+                if (existingUser) {
+                    return res.status(400).json({
+                        message: 'Sorry, that email is already registered to another account'
+                    });
+                }
+
+                const avatarUrl = gravatar.url(req.body.email, { s: '200', r: 'x', d: 'retro' }, true);
+
+                const user = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar: avatarUrl
+                });
+
+                user.save((err) => {
+                    if (err) {
+                        res.status(500).send(err);
+                    }
+                    
+                    user.password = undefined;
+                    res.status(201).send({
+                        id_token: token.generateJWT(user),
+                        user: user
+                    });
+                });
+            })
+            .catch(err => res.status(500).json(err));
+    },
+    
+    loginUser: (req, res) => {
+        User.findOne({ email: req.body.email })
+            .exec()
+            .then(user => {
+                if (!user) {
+                    return res.status(401).json({
+                        message: 'Wrong email address'
+                    });
+                }
+
+                user.comparePassword(req.body.password, (err, ismatch) => {
+                    if (!ismatch) {
+                        return res.status(401).json({
+                            message: 'Wrong password'
+                        });
+                    }
+
+                    user.password = undefined;
+                    res.status(201).json({
+                        id_token: token.generateJWT(user),
+                        user: user
+                    });
+                });
+            })
+            .catch(err => res.status(500).json(err));
+    }
 }
