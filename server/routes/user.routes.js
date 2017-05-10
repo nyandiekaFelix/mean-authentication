d853a8582539e586bda8be845780c9426a1cbcb8
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-10 12:37:19 +0300

    separate auth routes for modularity

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,17 +1,12 @@
 const router = require('express').Router();
 const userCtrl = require('../controllers/user.controller');
-const token = require('../../config/token');
 
 router.get('/', (req, res) => {
 	res.status(200).json({ message: 'User route works' });
 });
 
-router.post('/register', userCtrl.registerUser);
-router.post('/login', userCtrl.loginUser);
-
 router.route('/profile/:userId')
 	  .get(userCtrl.getOneUser)
 	  .put(userCtrl.updateUser);
-
 
 module.exports = router;
