c28670f4f2b6a67eb83e297f3717fb3e0fc67d4d
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-10 12:30:03 +0300

    move login and register methods to auth controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,75 +1,9 @@
 const gravatar = require('gravatar');
 
 const User = require('../models/user/user.model');
-const secrets = require('../config/secrets');
-const token = require('../config/passport');
 
 module.exports = {
-
-    // create a new user and store details in db
-    registerUser: (req, res) => {
-        User.findOne({ email: req.body.email })
-            .exec()
-            .then(existingUser => {
-                if (existingUser) {
-                    return res.status(400).json({
-                        message: 'Sorry, that email is already registered to another account'
-                    });
-                }
-
-                const avatarUrl = gravatar.url(req.body.email, { s: '200', r: 'x', d: 'retro' }, true);
-
-                const user = new User({
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password,
-                    avatar: avatarUrl
-                });
-
-                user.save((err) => {
-                    if (err) {
-                        res.status(500).send(err);
-                    }
-                    
-                    user.password = undefined;
-                    res.status(201).send({
-                        id_token: token.generateJWT(user),
-                        user: user
-                    });
-                });
-            })
-            .catch(err => res.status(500).json(err));
-    },
-
-    // Authenticate user using email and password before returning a token
-    loginUser: (req, res) => {
-        User.findOne({ email: req.body.email })
-            .exec()
-            .then(user => {
-                if (!user) {
-                    return res.status(401).json({
-                        message: 'Wrong email address'
-                    });
-                }
-
-                user.comparePassword(req.body.password, (err, ismatch) => {
-                    if (!ismatch) {
-                        return res.status(401).json({
-                            message: 'Wrong password'
-                        });
-                    }
-
-                    user.password = undefined;
-                    res.status(201).json({
-                        id_token: token.generateJWT(user),
-                        user: user
-                    });
-                });
-            })
-            .catch(err => res.status(500).json(err));
-    },
-
-    // Query database for current logged in user details
+    // Query database for a user's details
     getOneUser: (req, res) => {
         User.findById(req.params.userId, '-password')
             // .populate('books') 
