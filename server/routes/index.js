99554396169e56f11c05346749f5450c6d2430f9
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-15 19:54:53 +0300

    add auth routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./user.routes.js');
+const userRoutes = require('./user.routes');
+const authRoutes = require('./auth.routes');
 
 // All user endpoints to be preceded by '/user'
 router.use('/user', userRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
