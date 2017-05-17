7286b1c55b6c282cb08d12b6d53f052796d8d54d
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-17 12:12:24 +0300

    rename base user route url

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./user.routes');
 const authRoutes = require('./auth.routes');
 
 // All user endpoints to be preceded by '/user'
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 router.use('/auth', authRoutes);
 
 module.exports = router;
