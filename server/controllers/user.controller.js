567714cc03ee7375009edbc8600d9462a4ef8473
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-17 11:27:43 +0300

    add method to get all users

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,21 +3,34 @@ const gravatar = require('gravatar');
 const User = require('../models/user.model');
 
 module.exports = {
-    // Query database for a user's details
+    getAllUsers: (req, res) => {
+        User.find({})
+            .exec()
+            .then(users => {
+                if (!users) {
+                    return res.status(404).json({
+                        message: 'No users found'
+                    });
+                }
+                return res.status(200).json(users);
+            })
+            .catch(err => res.status(500).json(err));
+    },
+
     getOneUser: (req, res) => {
         User.findById(req.params.userId, '-password')
-            // .populate('books') 
             .exec()
             .then(user => {
                 if (!user) {
-                    return res.status(404).json({ message: 'User not found'});
+                    return res.status(404).json({ 
+                        message: 'User not found'
+                    });
                 }
                 return res.status(200).json(user);
             })
             .catch(err => res.status(500).json(err));
     },
 
-    // Update logged in user's details
     updateUser: (req, res) => {
          User.findOneAndUpdate(req.params.userId)
             .exec()
